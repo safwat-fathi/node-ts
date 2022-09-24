54f364c7915494710e82aa8142d4ecf8cd7460d7
Safwat Fathi <safwat.rashwan@gmail.com>
2022-09-24 16:25:50 +0200

    add: todo comment in categories schema

--- src/models/categories/categories.schema.ts
+++ src/models/categories/categories.schema.ts
@@ -2,6 +2,7 @@ import { Schema } from "mongoose";
 import { Category } from "types/db";
 
 export const categorySchema = new Schema<Category>({
+  // ! should implement add sub category id to parent category when its created
   name: { type: String, required: true },
   description: { type: String, required: true },
   sub: [{ type: Schema.Types.ObjectId, ref: "Category", required: true }],
