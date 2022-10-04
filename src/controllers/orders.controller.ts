fa8311df80b0c395c16ebe2a78fd2fb31ca508f3
Safwat Fathi <safwat.rashwan@gmail.com>
2022-10-04 07:23:08 +0200

    fix: order controller

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -2,13 +2,15 @@ import { Request, Response } from "express";
 import { UserModel } from "models/user/user.model";
 import { OrderModel } from "models/orders/orders.model";
 import { ProductModel } from "models/products/products.model";
-import { Product } from "types/db";
+import { Product, User } from "types/db";
 import { ObjectId } from "mongoose";
 
 export const addOrder = async (req: Request, res: Response) => {
-  const { products, user, total, address, delivery } = req.body;
-
-  console.log(products);
+  const {
+    products,
+    user,
+  }: { products: { product: ObjectId; quantity: number }[]; user: User } =
+    req.body;
 
   try {
     const orderOwner = await UserModel.findOne({ user });
@@ -18,38 +20,39 @@ export const addOrder = async (req: Request, res: Response) => {
       return;
     }
 
-    const orderProducts: Product[] = await Promise.all(
-      products.map(
-        async (item: Promise<{ product: ObjectId; quantity: number }>) =>
-          ProductModel.findOne({
-            _id: { $in: (await item).product },
-          })
-            .then(async (doc) => {
-              // doc?.stock = doc?.stock - (await item).product;
-              await doc?.updateOne({
-                stock: doc?.stock - (await item).quantity,
-              });
+    for (let i in products) {
+      const productId = products[i].product;
+      const qty = products[i].quantity;
 
-              await doc?.save();
-            })
-            .catch((err) => {
-              res.status(500).json({ message: err });
-              return;
-            })
-      )
-    );
+      const product: Product | null = await ProductModel.findOne({
+        _id: productId,
+      });
 
-    if (orderProducts && orderProducts.length === 0) {
-      res.status(404).json({ message: "No products found" });
-      return;
+      if (!product) {
+        res.status(404).json({
+          message: `Product not found`,
+        });
+        return false;
+      }
+
+      if (product && product.stock < qty) {
+        res.status(400).json({
+          message: `Product ${product.name} does not have enough stock`,
+        });
+        return false;
+      }
+
+      if (product) {
+        product.updateOne({ stock: product.stock - qty });
+
+        await product.save();
+      }
     }
 
     const newOrder = await OrderModel.create({
       user,
       products,
-      total,
-      address,
-      delivery,
+      ...req.body,
     });
 
     await newOrder.save();
