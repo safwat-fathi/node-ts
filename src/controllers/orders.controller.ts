5afab06323718f12b220f31e94500e0a41bb80f3
Safwat Fathi <safwat.rashwan@gmail.com>
2022-10-07 14:55:39 +0200

    fix: add new order

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 import { UserModel } from "models/user/user.model";
 import { OrderModel } from "models/orders/orders.model";
 import { ProductModel } from "models/products/products.model";
-import { Product, User } from "types/db";
-import { ObjectId } from "mongoose";
+import { Product, User, Order } from "types/db";
+// import { ObjectId } from "mongoose";
 
 export const addOrder = async (req: Request, res: Response) => {
   const {
     products,
     user,
-  }: { products: { product: ObjectId; quantity: number }[]; user: User } =
+  }: Order =
     req.body;
 
   try {
@@ -20,43 +20,56 @@ export const addOrder = async (req: Request, res: Response) => {
       return;
     }
 
+		
+		let orderTotal = 0
+		
     for (let i in products) {
       const productId = products[i].product;
       const qty = products[i].quantity;
-
       const product: Product | null = await ProductModel.findOne({
-        _id: productId,
+				_id: productId,
       });
-
+			
       if (!product) {
-        res.status(404).json({
-          message: `Product not found`,
+				res.status(404).json({
+					message: `Product not found`,
         });
         return false;
       }
-
+			
       if (product && product.stock < qty) {
         res.status(400).json({
           message: `Product ${product.name} does not have enough stock`,
         });
         return false;
       }
-
+			
       if (product) {
+				orderTotal += product.price
         product.updateOne({ stock: product.stock - qty });
-
+				
         await product.save();
       }
     }
 
+		// if (orderTotal !== total) {
+		// 	res.status(400).json({
+		// 		message: `Order total is not correct`,
+		// 	});
+		// 	return false;
+		// }
+		
     const newOrder = await OrderModel.create({
-      user,
+			user,
       products,
+			total: orderTotal,
       ...req.body,
     });
-
+		
+		// add order to user document
+		await orderOwner.updateOne({$push: {orders: newOrder.id}})
     await newOrder.save();
-
+		
     res
       .status(200)
       .json({ data: newOrder, message: "Order added successfully" });
