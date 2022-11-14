06dec54ad14a834617ba79c5d4ac57639a7bb4e9
Safwat Fathi <safwat.fathi@jobzella.com>
2022-11-14 17:31:07 +0200

    add: generateToken

--- src/models/user/user.model.ts
+++ src/models/user/user.model.ts
@@ -2,6 +2,7 @@ import { model } from "mongoose";
 import { User } from "types/db";
 import { userSchema } from "./user.schema";
 import { hashPassword } from "utils/auth";
+import { CustomJwtPayload } from "types/jwt";
 
 export const UserModel = model<User>("User", userSchema);
 
@@ -27,4 +28,6 @@ export class UserStore {
       throw new Error(`error user signup ${err}`);
     }
   }
+
+  async generateToken(): Promise<CustomJwtPayload> {}
 }
