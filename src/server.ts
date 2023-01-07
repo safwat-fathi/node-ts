7cc646573657f76c9c5fdf2c27a79c07a77ae7cf
Safwat Fathi <safwat.rashwan@gmail.com>
2023-01-07 20:15:51 +0200

    add: WS 9in progress)

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 import express, { Express } from "express";
 import compression from "compression";
 import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import session from "express-session";
 import { errorHandler } from "middlewares/error.middleware";
 import { connectDB } from "config/db.config";
 // seeders
@@ -16,7 +14,7 @@ import routes from "routes";
 dotenv.config();
 
 const app: Express = express();
-const port = <number>process.env.PORT || 8080;
+export const PORT = <number>process.env.PORT || 8080;
 
 // connect to DB
 connectDB();
@@ -29,23 +27,14 @@ seedProducts();
 app.use(compression());
 app.use(cors());
 app.use(express.json());
-// app.use(cookieParser());
-// app.use(
-//   session({
-//     secret,
-//     saveUninitialized: false,
-//     resave: false,
-//     cookie: { httpOnly: true, maxAge: 21600000 },
-//   })
-// );
 
 // routes
 app.use("/api", routes);
 //ErrorHandler (Should be last piece of middleware)
 app.use(errorHandler);
 
-const server = app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+const server = app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
 
 // cb function accepts two params error and promise
@@ -53,3 +42,5 @@ process.on("unhandledRejection", error => {
   console.log(`Logged Error: ${error}`);
   server.close(() => process.exit(1));
 });
+
+export default app;
