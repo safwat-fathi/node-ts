d8a541b5fbaad626e15753d462dfbd4790578390
Safwat Fathi <safwat.rashwan@gmail.com>
2023-01-27 16:24:20 +0200

    update: schema with required error custom message

--- src/models/orders/orders.schema.ts
+++ src/models/orders/orders.schema.ts
@@ -3,28 +3,36 @@ import { Order } from "types/db";
 import { geocoder } from "utils/geocoder";
 
 export const OrderSchema = new Schema<Order>({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  total: { type: Number, required: true },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "{VALUE} can not be null"],
+  },
+  total: { type: Number, required: [true, "{VALUE} can not be null"] },
   products: {
     type: [
       {
         product: {
           type: Schema.Types.ObjectId,
           ref: "Product",
-          required: true,
+          required: [true, "{VALUE} can not be null"],
           _id: false,
         },
         quantity: Number,
         _id: false,
       },
     ],
-    required: true,
+    required: [true, "{VALUE} can not be null"],
     validate: (val: { product: Schema.Types.ObjectId; quantity: number }[]) =>
       Array.isArray(val) && val.length > 0,
   },
-  status: { type: String, required: true },
-  address: { type: String, required: true },
-  delivery: { type: Date, default: Date.now, required: true },
+  status: { type: String, required: [true, "{VALUE} can not be null"] },
+  address: { type: String, required: [true, "{VALUE} can not be null"] },
+  delivery: {
+    type: Date,
+    default: Date.now,
+    required: [true, "{VALUE} can not be null"],
+  },
 });
 
 // geocode & create address field
